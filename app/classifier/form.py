f7379357f9617976c97ac2a0dfa2135ba656850c
ingsissam <33227372+ingsissam@users.noreply.github.com>
2022-08-10 16:50:54 -0400

    change the default  value to false

--- app/classifier/form.py
+++ app/classifier/form.py
@@ -6,7 +6,7 @@ from flask_login import current_user
 
 class RateForm(FlaskForm):
     radio = RadioField("Image Rating:" , choices=[(-3, "-3"), (-2, "-2"), (-1, "-1"), (0, "0"), (1, "1"),(2,"2"),(3,"3")], validators=[DataRequired()])
-    contentType = BooleanField("Inappropriate content type" ,default = True)
+    contentType = BooleanField("Inappropriate content" ,default = False)
     nextBtn = SubmitField('Next page')
 
 class DatePerformanceForm(FlaskForm):
